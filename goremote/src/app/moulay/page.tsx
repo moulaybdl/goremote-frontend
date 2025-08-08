96441433c1c6927de785fc0a3aa1e79eab75c4ca
moulaybdl <moulay.mohamed.bouabd.elli@ensia.edu.dz>
2025-08-08 23:05:54 +0200

    button2

--- goremote/src/app/moulay/page.tsx
+++ goremote/src/app/moulay/page.tsx
@@ -1,3 +1,4 @@
+import Button2 from "@/components/buttons/button2";
 import SimpleTag from "@/components/tags/simpleTag";
 
 export default function Moulay() {
@@ -5,7 +6,7 @@ export default function Moulay() {
     <div className="w-full h-full flex-col justify-start mt-10">
       {/* tags */}
       <div className="flex flex-col gap-5 ">
-        <span>General tags</span>
+        <span className="font-lexend-medium">General tags</span>
         <div className={`flex flex-row gap-2 `}>
           <SimpleTag title={"Expert"} color="bg-[#00FF5D]" />
           <SimpleTag title={"Intermediate"} color="bg-[#F6A93A]" />
@@ -14,12 +15,17 @@ export default function Moulay() {
       </div>
       {/* company type */}
       <div className="flex flex-col gap-5 mt-5">
-        <span>Company type</span>
+        <span className="font-lexend-medium">Company type</span>
         <div className={`flex flex-row gap-2 `}>
           <SimpleTag title={"Start-up"} color="bg-[#12AFE4]" />
           <SimpleTag title={"Non-profit"} color="bg-[#12AFE4]" />
           <SimpleTag title={"Corporation"} color="bg-[#12AFE4]" />
         </div>
+      </div>
+      {/* Buttons */}
+      <div className="flex flex-col gap-5 mt-5">
+        <div className="font-lexend-medium">Buttons</div>
+        <Button2 title="Company" />
       </div>
     </div>
   );
